0814d404ff9289630c96d5625d119ea46d8d32da
Leslie <lesliec38@hotmail.fr>
2021-06-03 17:11:08 +0200

    feat(controller/route/view): ajout d'une interface administrateur creation des composants qui s'y refere

---
+++ src/Form/PropertyType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+    // méthode permettant de construire le formulaire
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('price')
+            ->add('heat', ChoiceType::class, [
+                'choices' => $this->getChoice()
+            ])
+            ->add('city')
+            ->add('address')
+            ->add('postal_code')
+            ->add('sold');
+    }
+
+    // méthode permettant de définir les options du formulaire de manière globale
+    // on peut utiliser le système de traduction avec la fct translation_domain
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+            'translation_domain' => 'forms'
+        ]);
+    }
+
+    private function getChoice()
+    {
+        $choices = Property::HEAT;
+        $output = [];
+        foreach ($choices as $k => $v) {
+            $output[$v] = $k;
+        }
+        return $output;
+    }
+}
